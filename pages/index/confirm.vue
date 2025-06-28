f6f69d847bff48ea0518fb1f0ca4242f42343441
谢玲玉 <12297604+xie-lingyu@user.noreply.gitee.com>
2025-06-28 16:50:33 +0800

    核销码展示

--- pages/index/confirm.vue
+++ pages/index/confirm.vue
@@ -1,16 +1,26 @@
 <template>
     <view class="item-wrap">
-        <view class="image-content">
-            <view class="img-wrap">
-                <image style="width: 400rpx; height: 400rpx; background-color: #fff;" :mode="'aspectFit'" :src="imgPath"
-                ></image>
+        <view v-if="!showQrCode" >
+            <view class="image-content">
+                <view class="img-wrap">
+                    <image style="width: 400rpx; height: 400rpx; background-color: #fff;" :mode="'aspectFit'" :src="imgPath"
+                    ></image>
+                </view>
+            </view>
+            <button type="primary" @click="submit">确定制作</button>
+        </view>
+        <view class="qrcode-wrap" v-else>
+            <view class="qrcode-content">
+                <view class="code-title">核销码</view>
+                <uv-qrcode ref="qrcode" canvas-id="qrcode" :value="qrValue" size="300rpx" :options="options"></uv-qrcode>
+                <view>将核销码扣至扫码区，开始制作</view>
             </view>
         </view>
-        <button type="primary" @click="submit">确定制作</button>
     </view>
 </template>
 <script>
-import { checkCode, login } from '../../api'
+import uvQrcode from '@/uni_modules/uv-qrcode/components/uv-qrcode/uv-qrcode.vue';
+
 export default {
     onLoad(query) {
         this.imgPath = decodeURIComponent(query.img || '');
@@ -19,10 +29,23 @@ export default {
     data() {
         return {
             imgPath: "",
+            qrValue: "http://www.baidu.com",
+            showQrCode: false,
+            options: {
+                useDynamicSize: false,
+                errorCorrectLevel: 'Q',
+                margin: 10,
+                areaColor: "#fff",
+                // 指定二维码前景，一般可在中间放logo
+                // foregroundImageSrc: require('static/image/logo.png')
+            }
+
         }
     },
     methods: {
-        submit() { }
+        submit() {
+            this.showQrCode = true;
+        }
 
     }
 }
@@ -41,6 +64,20 @@ export default {
 
     .input-wrap {
         margin: 30rpx 0;
+    }
+
+    .qrcode-wrap {
+        width: 100%;
+        text-align: center;
+        margin: 0 auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 400rpx; // 可根据实际二维码高度调整
+
+        .code-title {
+            color: #696565;
+        }
     }
 }
 </style>
