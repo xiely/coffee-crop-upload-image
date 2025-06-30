33b0626d9ac026c833ea30ffc3dd0da693d120a5
谢玲玉 <12297604+xie-lingyu@user.noreply.gitee.com>
2025-06-30 15:42:59 +0800

    优化代码

--- pages/index/confirm.vue
+++ pages/index/confirm.vue
@@ -1,19 +1,19 @@
 <template>
     <view class="item-wrap">
-        <view v-if="!showQrCode" >
+        <view v-if="!showQrCode" class="upload-img-wrap">
             <view class="image-content">
                 <view class="img-wrap">
-                    <image style="width: 400rpx; height: 400rpx; background-color: #fff;" :mode="'aspectFit'" :src="imgPath"
+                    <image class="image" :mode="'aspectFit'" :src="imgPath"
                     ></image>
                 </view>
             </view>
-            <button type="primary" @click="submit">确定制作</button>
+            <button class="sure-make" type="primary" @click="submit">确定制作</button>
         </view>
         <view v-else class="qrcode-wrap">
             <view class="qrcode-content">
                 <view class="code-title">核销码</view>
-                <uv-qrcode ref="qrcode" canvas-id="qrcode" :value="qrValue" size="300rpx" :options="options"></uv-qrcode>
-                <view>将核销码扣至扫码区，开始制作</view>
+                <uv-qrcode ref="qrcode" canvas-id="qrcode" :value="qrValue" size="400rpx" :options="options"></uv-qrcode>
+                <view class="qrcode-tip">将核销码扣至扫码区，开始制作</view>
             </view>
         </view>
     </view>
@@ -68,14 +68,39 @@ export default {
     align-items: center;
     min-height: 80vh;
 
-    .img-wrap {
-        width: 100%;
-        text-align: center;
-        margin: 20px
-    }
+    .upload-img-wrap {
+        .image-content {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            min-height: 400rpx; // 可根据实际图片高度调整
+            width: 400rpx;
+            margin: 0 auto;
 
-    .input-wrap {
-        margin: 30rpx 0;
+            .img-wrap {
+                width: 100%;
+                text-align: center;
+                margin: 20px;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+
+                .image {
+                    width: 400rpx;
+                    height: 400rpx;
+                    background-color: #fff;
+                    display: block;
+                }
+            }
+        }
+
+        .sure-make {
+            width: 400rpx;
+            margin: 0 auto 20px auto;
+            display: block;
+        }
+
     }
 
     .qrcode-wrap {
@@ -92,11 +117,23 @@ export default {
             flex-direction: column;
             align-items: center;
             width: 100%;
+
+            .code-title {
+                color: #696565;
+            }
+
+            .qrcode-tip {
+                width: 400rpx;
+                text-align: center;
+                margin-top: 16rpx;
+                font-size: 28rpx;
+                color: #333;
+            }
         }
 
-        .code-title {
-            color: #696565;
-        }
+
     }
+
+
 }
 </style>
